aeea2737b572873bfc88d81a2c1c6ee3d4b356b9
Ryan Wise <ryan@ryanwise.me>
2021-04-07 21:43:17 -0700

    imple Engine updates

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -9,22 +9,43 @@ pub struct Engine {
     active: bool,
 }
 
-pub fn initialize_game_engine() -> Result<Engine, JsValue> {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+impl Engine {
+     pub(crate) fn initialize_game_engine() -> Result<Engine, JsValue> {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let canvas = document.get_element_by_id("canvas").unwrap();
+        let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
 
-    let context = canvas
-        .get_context("webgl")?
-        .unwrap()
-        .dyn_into::<WebGlRenderingContext>()?;
+        let context = canvas
+            .get_context("webgl")?
+            .unwrap()
+            .dyn_into::<WebGlRenderingContext>()?;
 
-    if (true) {
-        Ok(Engine {
-            gl_context: context,
-            active: true
-        })
-    } else {
-        Err(JsValue::from("fuck me".to_string()))
+        if true {
+            Ok(Engine {
+                gl_context: context,
+                active: true
+            })
+        } else {
+            Err(JsValue::from("fuck me".to_string()))
+        }
+    }
+
+    pub(crate) fn initialize_shaders(&self, frag_source: &str, vert_source: &str) -> Result<JsValue, String> {
+        let vert_shader = shaders::compile_shader(
+            &self.gl_context,
+            WebGlRenderingContext::VERTEX_SHADER,
+            vert_source
+        )?;
+        let frag_shader = shaders::compile_shader(
+            &self.gl_context,
+            WebGlRenderingContext::FRAGMENT_SHADER,
+            frag_source
+        )?;
+
+        let program = shaders::link_program(&self.gl_context, &vert_shader, &frag_shader)?;
+        self.gl_context.use_program(Some(&program));
+
+        Ok(self.gl_context.get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS))
     }
 }
+
--- src/lib.rs
+++ src/lib.rs
@@ -7,26 +7,14 @@ use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader};
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
-    let ENGINE: Engine = initialize_game_engine().expect("Rip");
+    let engine = Engine::initialize_game_engine().expect("Rip");
 
-    let vert_shader = shaders::compile_shader(
-        &(ENGINE.gl_context),
-        WebGlRenderingContext::VERTEX_SHADER,
-        include_str!("shaders/vert.vs"),
-
-    )?;
-    let frag_shader = shaders::compile_shader(
-        &(ENGINE.gl_context),
-        WebGlRenderingContext::FRAGMENT_SHADER,
-        include_str!("shaders/frag.fs"),
-    )?;
-    let program = shaders::link_program(&(ENGINE.gl_context), &vert_shader, &frag_shader)?;
-    (ENGINE.gl_context).use_program(Some(&program));
+    engine.initialize_shaders( include_str!("./shaders/frag.fs"), include_str!("./shaders/vert.vs")).expect("oof");
 
     let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
 
-    let buffer = (ENGINE.gl_context).create_buffer().ok_or("failed to create buffer")?;
-    (ENGINE.gl_context).bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+    let buffer = (engine.gl_context).create_buffer().ok_or("failed to create buffer")?;
+    (engine.gl_context).bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
 
     // Note that `Float32Array::view` is somewhat dangerous (hence the
     // `unsafe`!). This is creating a raw view into our module's
@@ -39,20 +27,20 @@ pub fn start() -> Result<(), JsValue> {
     unsafe {
         let vert_array = js_sys::Float32Array::view(&vertices);
 
-        (ENGINE.gl_context).buffer_data_with_array_buffer_view(
+        (engine.gl_context).buffer_data_with_array_buffer_view(
             WebGlRenderingContext::ARRAY_BUFFER,
             &vert_array,
             WebGlRenderingContext::STATIC_DRAW,
         );
     }
 
-    (ENGINE.gl_context).vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
-    (ENGINE.gl_context).enable_vertex_attrib_array(0);
+    (engine.gl_context).vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
+    (engine.gl_context).enable_vertex_attrib_array(0);
 
-    (ENGINE.gl_context).clear_color(0.0, 1.0, 0.0, 1.0);
-    (ENGINE.gl_context).clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+    (engine.gl_context).clear_color(0.0, 1.0, 0.0, 1.0);
+    (engine.gl_context).clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
 
-    (ENGINE.gl_context).draw_arrays(
+    (engine.gl_context).draw_arrays(
         WebGlRenderingContext::TRIANGLES,
         0,
         (vertices.len() / 3) as i32,

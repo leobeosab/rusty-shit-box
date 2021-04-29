9c6b8d2266e85447913979630eedf5e99a61e4f3
Ryan Wise <ryan@ryanwise.me>
2021-04-29 04:00:54 -0400

    Working multiple cubes

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,11 +1,13 @@
 pub(crate) mod shaders;
 mod camera;
+mod cube;
 
 use wasm_bindgen::{JsCast, JsValue};
 use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlBuffer};
 use std::collections::HashMap;
 use gl_matrix::mat4;
 use crate::engine::camera::Camera;
+use crate::engine::cube::Cube;
 
 pub struct Engine {
     pub(crate) gl_context: WebGlRenderingContext,
@@ -80,68 +82,9 @@ impl Engine {
         self.gl_context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT | WebGlRenderingContext::DEPTH_BUFFER_BIT);
 
         let program = self.fetch_shader("simple_shader");
+        self.gl_context.use_program(Some(program));
 
-        let mut internal_model_view_matrix = mat4::create();
-        let mut internal_model_view_matrix_2 = internal_model_view_matrix.clone();
-
-        mat4::translate(
-            &mut internal_model_view_matrix,
-            &mut internal_model_view_matrix_2,
-            &[-0.0, 0.0, -6.0]
-        );
-
-        internal_model_view_matrix_2 = internal_model_view_matrix.clone();
-
-        mat4::rotate(
-            &mut internal_model_view_matrix,
-            &mut internal_model_view_matrix_2,
-            rotation,
-            &[0.0, 0.0, 1.0]
-        );
-
-        // We need to copy after _every_ translation/rotation -_-
-        internal_model_view_matrix_2 = internal_model_view_matrix.clone();
-
-        mat4::rotate(
-            &mut internal_model_view_matrix,
-            &mut internal_model_view_matrix_2,
-            rotation * 0.7,
-            &[0.0, 1.0, 0.0]
-        );
-
-        let vertex_position = self.gl_context.get_attrib_location(&program, "aVertexPosition");
-        let vertex_color = self.gl_context.get_attrib_location(&program, "aVertexColor");
         let projection_matrix = self.gl_context.get_uniform_location(&program, "uProjectionMatrix").unwrap();
-        let model_view_matrix = self.gl_context.get_uniform_location(&program, "uModelViewMatrix").unwrap();
-
-        let buffers = init_buffers(&self.gl_context)?;
-
-        self.gl_context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffers.position));
-        self.gl_context.vertex_attrib_pointer_with_f64(
-            vertex_position as u32,
-            3,
-            WebGlRenderingContext::FLOAT,
-            false,
-            0,
-            0.0
-        );
-
-        self.gl_context.enable_vertex_attrib_array(vertex_position as u32);
-
-        self.gl_context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffers.color));
-        self.gl_context.vertex_attrib_pointer_with_f64(
-            vertex_color as u32,
-            4,
-            WebGlRenderingContext::FLOAT,
-            false,
-            0,
-            0.0
-        );
-
-        self.gl_context.enable_vertex_attrib_array(vertex_color as u32);
-
-        self.gl_context.bind_buffer(WebGlRenderingContext::ELEMENT_ARRAY_BUFFER, Some(&buffers.indices));
-        self.activate_shader(program);
 
         // Setup the camera matrix
         self.gl_context.uniform_matrix4fv_with_f32_array(
@@ -150,158 +93,20 @@ impl Engine {
             &self.camera.projection_matrix,
         );
 
-        self.gl_context.uniform_matrix4fv_with_f32_array(
-            Some(&model_view_matrix),
-            false,
-            &internal_model_view_matrix
-        );
+        let mut cube = Cube::new(&self.gl_context);
 
-        self.gl_context.draw_elements_with_f64(
-            WebGlRenderingContext::TRIANGLES,
-            36,
-            WebGlRenderingContext::UNSIGNED_SHORT,
-            0.0
-        );
+        cube.translate(2.0, 0.0, -6.0);
+        cube.rotate(rotation, rotation, rotation * 0.8);
+
+        let mut cube2 = Cube::new(&self.gl_context);
+        cube2.translate(-2.0, 0.0, -6.0);
+        cube2.rotate(rotation, 0.0, rotation * 0.2);
+
+        let mut offset = cube.draw(&self.gl_context, program, 0.0);
+        offset += cube2.draw(&self.gl_context, program, 0.0);
 
         Ok(JsValue::from("Success!"))
     }
 }
 
 
-fn init_buffers(context: &WebGlRenderingContext) -> Result<Buffers, String> {
-    // Setup the positions buffer
-
-    let position_buffer = context.create_buffer().ok_or("failed to create position buffer")?;
-    context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&position_buffer));
-
-    let positions: [f32; 72] = [
-        // Front face
-        -1.0, -1.0,  1.0,
-        1.0, -1.0,  1.0,
-        1.0,  1.0,  1.0,
-        -1.0,  1.0,  1.0,
-
-        // Back face
-        -1.0, -1.0, -1.0,
-        -1.0,  1.0, -1.0,
-        1.0,  1.0, -1.0,
-        1.0, -1.0, -1.0,
-
-        // Top face
-        -1.0,  1.0, -1.0,
-        -1.0,  1.0,  1.0,
-        1.0,  1.0,  1.0,
-        1.0,  1.0, -1.0,
-
-        // Bottom face
-        -1.0, -1.0, -1.0,
-        1.0, -1.0, -1.0,
-        1.0, -1.0,  1.0,
-        -1.0, -1.0,  1.0,
-
-        // Right face
-        1.0, -1.0, -1.0,
-        1.0,  1.0, -1.0,
-        1.0,  1.0,  1.0,
-        1.0, -1.0,  1.0,
-
-        // Left face
-        -1.0, -1.0, -1.0,
-        -1.0, -1.0,  1.0,
-        -1.0,  1.0,  1.0,
-        -1.0,  1.0, -1.0,
-    ];
-
-    // More info on why this is unsafe
-    // https://docs.rs/js-sys/0.3.45/js_sys/struct.Float32Array.html#unsafety
-    unsafe {
-        let position_array = js_sys::Float32Array::view(&positions);
-
-        context.buffer_data_with_array_buffer_view(
-            WebGlRenderingContext::ARRAY_BUFFER,
-            &position_array,
-            WebGlRenderingContext::STATIC_DRAW
-        )
-    }
-
-    // Setup the color buffer
-
-    let colors: [f32; 96] = [
-        1.0,  1.0,  1.0,  1.0,    // Front face: white
-        1.0,  1.0,  1.0,  1.0,    // Front face: white
-        1.0,  1.0,  1.0,  1.0,    // Front face: white
-        1.0,  1.0,  1.0,  1.0,    // Front face: white
-        1.0,  0.0,  0.0,  1.0,    // Back face: red
-        1.0,  0.0,  0.0,  1.0,    // Back face: red
-        1.0,  0.0,  0.0,  1.0,    // Back face: red
-        1.0,  0.0,  0.0,  1.0,    // Back face: red
-        0.0,  1.0,  0.0,  1.0,    // Top face: green
-        0.0,  1.0,  0.0,  1.0,    // Top face: green
-        0.0,  1.0,  0.0,  1.0,    // Top face: green
-        0.0,  1.0,  0.0,  1.0,    // Top face: green
-        0.0,  0.0,  1.0,  1.0,    // Bottom face: blue
-        0.0,  0.0,  1.0,  1.0,    // Bottom face: blue
-        0.0,  0.0,  1.0,  1.0,    // Bottom face: blue
-        0.0,  0.0,  1.0,  1.0,    // Bottom face: blue
-        1.0,  1.0,  0.0,  1.0,    // Right face: yellow
-        1.0,  1.0,  0.0,  1.0,    // Right face: yellow
-        1.0,  1.0,  0.0,  1.0,    // Right face: yellow
-        1.0,  1.0,  0.0,  1.0,    // Right face: yellow
-        1.0,  0.0,  1.0,  1.0,    // Left face: purple
-        1.0,  0.0,  1.0,  1.0,    // Left face: purple
-        1.0,  0.0,  1.0,  1.0,    // Left face: purple
-        1.0,  0.0,  1.0,  1.0,    // Left face: purple
-    ];
-
-
-    let color_buffer = context.create_buffer().ok_or("failed to create color buffer")?;
-    context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&color_buffer));
-
-    // More info on why this is unsafe
-    // https://docs.rs/js-sys/0.3.45/js_sys/struct.Float32Array.html#unsafety
-    unsafe {
-        let color_array = js_sys::Float32Array::view(&colors);
-
-        context.buffer_data_with_array_buffer_view(
-            WebGlRenderingContext::ARRAY_BUFFER,
-            &color_array,
-            WebGlRenderingContext::STATIC_DRAW
-        )
-    }
-
-    let index_buffer = context.create_buffer().ok_or("failed to create index buffer")?;
-    context.bind_buffer(WebGlRenderingContext::ELEMENT_ARRAY_BUFFER, Some(&index_buffer));
-
-    let indices = [
-        0,  1,  2,      0,  2,  3,    // front
-        4,  5,  6,      4,  6,  7,    // back
-        8,  9,  10,     8,  10, 11,   // top
-        12, 13, 14,     12, 14, 15,   // bottom
-        16, 17, 18,     16, 18, 19,   // right
-        20, 21, 22,     20, 22, 23,   // left
-    ];
-
-    // More info on why this is unsafe
-    // https://docs.rs/js-sys/0.3.45/js_sys/struct.Float32Array.html#unsafety
-    unsafe {
-        let index_array = js_sys::Uint16Array::view(&indices);
-
-        context.buffer_data_with_array_buffer_view(
-            WebGlRenderingContext::ELEMENT_ARRAY_BUFFER,
-            &index_array,
-            WebGlRenderingContext::STATIC_DRAW
-        )
-    }
-
-    Ok(Buffers {
-        position: position_buffer,
-        color: color_buffer,
-        indices: index_buffer
-    })
-}
-
-struct Buffers {
-    position: WebGlBuffer,
-    color: WebGlBuffer,
-    indices: WebGlBuffer,
-}
